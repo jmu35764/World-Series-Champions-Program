5f408414eff3553b3f37bc1c05a0538b7edf57c7
jm underwood <jm.underwood@hotmail.com>
2025-07-30 17:17:13 -0400

    Removed test code.  Need to add comments later.

--- World Series Champions Program.cpp
+++ World Series Champions Program.cpp
@@ -16,35 +16,56 @@ void getTeams(string filename1, string filename2, vector<string>& teams, string 
 
     if (!File1 && !File2)
     {
-        std::cerr << "Error Opening File!" << std::endl;
-        //return 1;
+        cerr << "Error Opening File!" << std::endl;
     }
 
    
     while (getline(File1, team))
     {
         teams.push_back(team);
-        //cout << teams[x] << endl;
-        //x++;
-
     }
     x = 0;
 
-    cout << endl;
-
     while (getline(File2, win))
     {
-        //cout << "Test" << endl;
         winners.push_back(win);
-       // cout << winners[x] << endl;
-        //x++;
     }
 
 }
 
+void findWinner(string teamname, vector<string> winners)
+{
+     while (teamname != "quit")
+    {
+        int counter = 0;
+        int y = 0;
+        cout << "Enter the name of a team (enter quit to end):" << endl;
+        getline(cin, teamname);
+        cout << endl;
+
+        for (string val : winners)
+        {
+            if (teamname == winners[y])
+            {
+                counter++;
+            }
+             y++;
+        }
+        if (teamname != "quit")
+        {
+            cout << teamname << " won the World Series " << counter << " times." << endl;
+        }
+          
+        else
+        {
+            cout << endl;
+        }
+    }
+}
+
+    
 int main()
 {
-    //int x = 0;
     string filename1 = "Teams.txt";
     string filename2 = "WorldSeriesWinners.txt";
     vector<string> Teams;
@@ -52,40 +73,8 @@ int main()
     vector<string> Winners;
     string Winner;
     string teamName;
-    //int counter = 0;
     
     getTeams(filename1, filename2, Teams, Team, Winners, Winner);
+    findWinner(teamName, Winners);
 
-    while (teamName != "quit")
-    {
-        int counter = 0;
-        int x = 0;
-        cout << "Enter Team Name" << endl;
-        getline(cin, teamName);
-
-        for (string val : Winners)
-        {
-            //cout << "test" << endl;
-            if (teamName == Winners[x])
-            {
-                //cout << "test" << endl;
-                counter++;
-            }
-            x++;
-        }
-
-        cout << teamName << " won the World Series " << counter << " times." << endl;
-    }
-    
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
